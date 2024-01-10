032a14b3526d6b625ca5ff3a1eb6550605b6a4a7
Attila732 <kovacsattila478@gmail.com>
2024-01-10 10:55:24 +0100

    teszt2

--- src/test/java/test/java/Romb.java
+++ src/test/java/test/java/Romb.java
@@ -1,9 +1,15 @@
 package test.java;
 
+import java.util.InputMismatchException;
+
 public class Romb {
 
-    public double calcRadius(int i, int j) {
-        return 0;
+        public double calcRadius(double sideA, double angle) {
+        if (sideA <= 0 || angle <= 0) {
+            throw new InputMismatchException();
+        }
+        return Math.pow(sideA, 2) * Math.sin(angle);
     }
-
 }
+
+
