eaaea13598211e50fc892d8a305df0d01bd1fde3
Attila732 <kovacsattila478@gmail.com>
2024-01-10 11:02:11 +0100

    teszt3

--- src/test/java/test/java/rombtest.java
+++ src/test/java/test/java/rombtest.java
@@ -13,24 +13,24 @@ public class rombtest {
         this.romb = new Romb();
     }
 
-    @Test
+    @Test(expectedExceptions = InputMismatchException.class)
     public void testCalcMinus_minus30_35() {
         double actual = this.romb.calcRadius(-30, 35);
-        double expected = 385.364402547;
-        Assert.assertEquals(actual, expected, 0.1);
-    }
-    @Test
-    public void testCalcMinusDegree_48_minus36() {
-        double actual = this.romb.calcRadius(48, -36);
-        double expected = 2285.05847833;
+        double expected = 385.36;
         Assert.assertEquals(actual, expected, 0.1);
     }
     @Test(expectedExceptions = InputMismatchException.class)
-    public void testCalcRadius_Zero_Side() {
+    public void testCalcMinusangle_48_minus36() {
+        double actual = this.romb.calcRadius(48, -36);
+        double expected = 2285.05;
+        Assert.assertEquals(actual, expected, 1);
+    }
+    @Test(expectedExceptions = InputMismatchException.class)
+    public void testCalc_Zero_Side() {
         this.romb.calcRadius(0, 40);
     }
     @Test(expectedExceptions = InputMismatchException.class)
-    public void testCalcRadius_Zero_Angle() {
+    public void testCalc_Zero_Angle() {
         this.romb.calcRadius(1, 0);
     }
 }
